532876d33e617b85ff36919da334fd63b991dc82
walk <brew@atheist.com>
2023-06-30 23:36:30 -0400

    FIXED UR PROBLEM BUDDY

--- src/main/java/dev/lbuddyboy/flash/FlashLanguage.java
+++ src/main/java/dev/lbuddyboy/flash/FlashLanguage.java
@@ -150,7 +150,7 @@ public enum FlashLanguage {
             "heresaGift",
             "zaqhy",
             "Serversided",
-            "ToadLw",
+            "ToadL",
             "wisi",
             "darnell42"
     )),
